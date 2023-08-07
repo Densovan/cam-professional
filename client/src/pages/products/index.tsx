82972348bd2d3793d5372b3702826b9f3f3a2081
Thith-thith <thiththin762@gmail.com>
2023-08-07 05:10:56 -0400

    Changes to be committed: 	modified:   client/src/pages/products/index.tsx

--- client/src/pages/products/index.tsx
+++ client/src/pages/products/index.tsx
@@ -698,41 +698,41 @@ const Products: Component<{}> = () => {
 
 export default Products;
 
-export const Banner: Component = () => {
-  return (
-    <div class="container mx-auto pt-9 md:pt-12 md:px-3">
-      <div class="flex items-strech justify-center flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 lg:space-x-4">
-        <div class="rounded-2xl flex flex-col md:flex-row items-strech justify-between bg-gray-50 dark:bg-gray-800 py-6 px-6 md:py-12 lg:px-12 md:w-8/12 lg:w-7/12 xl:w-8/12 2xl:w-9/12">
-          <div class="flex flex-col justify-center md:w-1/2">
-            <h1 class="text-3xl lg:text-4xl font-semibold text-gray-800 dark:text-white">
-              Best Deal
-            </h1>
-            <p class="text-base lg:text-xl text-gray-800 dark:text-white mt-2">
-              Save upto <span class="font-bold">50%</span>
-            </p>
-          </div>
-          <div class="md:w-1/2 mt-8 md:mt-0 flex justify-center md:justify-end">
-            <img src="/products/product-2.png" alt="" class="w-1/2" />
-          </div>
-        </div>
-        <div class="rounded-2xl md:w-4/12 lg:w-5/12 xl:w-4/12 2xl:w-3/12 bg-gray-50 dark:bg-gray-800 py-6 px-6 md:py-0 md:px-4 lg:px-6 flex flex-col justify-center relative">
-          <div class="flex flex-col justify-center">
-            <h1 class="text-3xl lg:text-4xl font-semibold text-gray-800 dark:text-white">
-              RIBBON
-            </h1>
-            <p class="text-base lg:text-xl text-gray-800 dark:text-white">
-              Save Upto <span class="font-bold">30%</span>
-            </p>
-          </div>
-          <div class="flex justify-end md:absolute md:bottom-4 md:right-4 lg:bottom-0 lg:right-0">
-            <img
-              src="/products/product-2.png"
-              alt=""
-              class="md:w-12 md:h-12 lg:w-1/2 lg:h-1/2"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+// export const Banner: Component = () => {
+//   return (
+//     <div class="container mx-auto pt-9 md:pt-12 md:px-3">
+//       <div class="flex items-strech justify-center flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 lg:space-x-4">
+//         <div class="rounded-2xl flex flex-col md:flex-row items-strech justify-between bg-gray-50 dark:bg-gray-800 py-6 px-6 md:py-12 lg:px-12 md:w-8/12 lg:w-7/12 xl:w-8/12 2xl:w-9/12">
+//           <div class="flex flex-col justify-center md:w-1/2">
+//             <h1 class="text-3xl lg:text-4xl font-semibold text-gray-800 dark:text-white">
+//               Best Deal
+//             </h1>
+//             <p class="text-base lg:text-xl text-gray-800 dark:text-white mt-2">
+//               Save upto <span class="font-bold">50%</span>
+//             </p>
+//           </div>
+//           <div class="md:w-1/2 mt-8 md:mt-0 flex justify-center md:justify-end">
+//             <img src="/products/product-2.png" alt="" class="w-1/2" />
+//           </div>
+//         </div>
+//         <div class="rounded-2xl md:w-4/12 lg:w-5/12 xl:w-4/12 2xl:w-3/12 bg-gray-50 dark:bg-gray-800 py-6 px-6 md:py-0 md:px-4 lg:px-6 flex flex-col justify-center relative">
+//           <div class="flex flex-col justify-center">
+//             <h1 class="text-3xl lg:text-4xl font-semibold text-gray-800 dark:text-white">
+//               RIBBON
+//             </h1>
+//             <p class="text-base lg:text-xl text-gray-800 dark:text-white">
+//               Save Upto <span class="font-bold">30%</span>
+//             </p>
+//           </div>
+//           <div class="flex justify-end md:absolute md:bottom-4 md:right-4 lg:bottom-0 lg:right-0">
+//             <img
+//               src="/products/product-2.png"
+//               alt=""
+//               class="md:w-12 md:h-12 lg:w-1/2 lg:h-1/2"
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
